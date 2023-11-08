a09702f2d99ce90501a7d999e4ec69d3f9149930
Leonardo Vučenović <101517924+leonardo-vucenovic@users.noreply.github.com>
2023-11-08 11:03:43 +0100

    Update NastavniPlanTable.js

--- NastavniPlanTable.js
+++ NastavniPlanTable.js
@@ -127,7 +127,9 @@ function DeleteSubjectFromTable(button, kolegij)
     Sum();
   });
 }
+/*
 function ChechPage(){
   if(localStorage.getItem("token") === null);
   window.location.href = "Pocetna.html";
 }
+*/
